7f1b5b932a64a992b67fdd36262544a39f5fc4d8
Dwi Susanto <massusant@gmail.com>
2020-05-27 15:37:11 +0700

    Initial Commit

---
+++ CreateListMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CreateListMenu
+//
+//  Created by Dwi Susanto on 27/05/20.
+//  Copyright © 2020 Kelana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
